269172231a6359f696280deccab49e85a295ec0c
rickhung76 <rickhung76@gmail.com>
2017-02-15 16:30:17 +0800

    Initial Commit

---
+++ PositiveGridDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PositiveGridDemo
+//
+//  Created by Rick on 2017/2/15.
+//  Copyright © 2017年 Rick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
